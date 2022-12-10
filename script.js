26ecb7a7f4c6116ce6a68e54dcaa2feaa03c048d
eashirokovaa <eashirokovaa@gmail.com>
2022-12-10 20:59:48 +0300

    HW

---
+++ script.js
@@ -0,0 +1,17 @@
+function onSearch() {
+  let request = document.getElementById("searchform").value;
+  fetch(
+    "https://api.giphy.com/v1/gifs/search?api_key=ubRd9RWAMEfiWz7JoWIKwS8xEcbZxQdJ&q=" +
+      request +
+      "&limit=5&offset=0&rating=g&lang=en"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      gifs = "";
+      for (let i = 0; i < data.data.length ; i++) {
+        gifs += `<img src="${data.data[i].images.fixed_width.url}"/>`;
+      }
+      document.getElementById("gif").innerHTML = gifs;
+    })
+    .catch((err) => console.log(err));
+}
